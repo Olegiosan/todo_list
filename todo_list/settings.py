c4e7a3bbe0d52b5145a820240ae455271bb61824
Olegiosan <olegyurkosan@gmail.com>
2025-08-30 13:26:12 -0500

    version 2.1.2

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9&2m#71nuvxx!@cefw%(uz^d3fm^r_gnc4v1x0t$fvrc!v5r76
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
